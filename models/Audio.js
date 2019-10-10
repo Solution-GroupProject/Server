75f8e2b542a202f5c4a79a00954d8c8b5e9aa431
arnold_therigan <arnoldtherigan15@gmail.com>
2019-10-10 21:13:19 +0700

    gcp finish by aldi

--- models/Audio.js
+++ models/Audio.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema
 
 let audioSchema = new Schema({
     locationGcp : {type:String},
-    comment : {type:String},
+    comment : {type:String}    
 },{timestamps:true})
 
 let Audio = mongoose.model('Audio',audioSchema)
--- routes/audioRouter.js
+++ routes/audioRouter.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 const AudioController = require('../controllers/AudioController')
+const multerHelp = require('../helpers/multerHelp')
 
 
-router.post('/',AudioController.addAudio)
+// router.post('/',AudioController.addAudio)
+router.post('/',  multerHelp.multer.single('audio'), multerHelp.sendUploadToGCS,AudioController.addAudio)
+
+// router.post('/',  multerHelp.multer.single('audio'), multerHelp.sendUploadToGCS, (req,res) => {
+//     res.send({
+//         status: 200,
+//         message: 'Your file is successfully uploaded',
+//         link: req.file.cloudStoragePublicUrl
+//     })
+// })
 
 module.exports =router
